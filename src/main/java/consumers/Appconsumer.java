200dea0e05cf0c3ee9e0cb964499cc4781001d0b
991939308 <991939308@qq.com>
2018-09-02 14:52:00 +0800

    注释

--- src/main/java/consumers/Appconsumer.java
+++ src/main/java/consumers/Appconsumer.java
@@ -4,6 +4,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Appconsumer {
+    /*
+    *   订阅模式
+    * 这里是让Spring去管理消费者的
+    *
+    * */
     public static void main(String[] arge){
         ApplicationContext context = new ClassPathXmlApplicationContext("consumer.xml");
 
